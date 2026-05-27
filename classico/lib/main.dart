adc803cd7139e9b74f511db1078f156eb37a0535
nalin <nalinikantbastia@gmail.com>
2026-05-27 20:44:02 +0530

    buttons and types of buttons in flutter

--- classico/lib/main.dart
+++ classico/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,52 +36,59 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
 
-      body: Center(
-        child: Container(
-          width: 500,
-          height: 600,
-          color: Colors.blue,
-          child: const Center(
-            child: Text('Box', style: TextStyle(color: Colors.white)),
-          ),
-        ),
+      // this is font changes in flutter
+      // body: Text('Hello Flutter docs', style: TextStyle(
+      //   fontSize: 25,
+      //   color: Colors.lightBlue,
+      //   fontWeight: FontWeight.bold,
+      //   backgroundColor: Colors.orangeAccent
+      // ),)
+
+        
+        //button types and functions.........
+    //   body: TextButton(
+    //     child: Text('Click Here'),
+    //     onPressed: (){
+    //       print('Button Clicked');
+    //     },
+    //     onLongPress: (){
+    //       print('Long Press');
+    //     },
+    //   )
+        
+
+      //elevated button
+        // body: ElevatedButton(
+        //   child: Text('Elevated button'),
+        //   onPressed: (){
+        //     print('Button Clicked');
+        //   },
+        // )
+    
+      
+      //outline button
+      body: OutlinedButton(
+        child: Text('Outlined button'),
+        onPressed: (){
+          print('Button Clicked');
+        },
+      )
 
 
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
